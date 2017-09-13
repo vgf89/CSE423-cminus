37d1e54c16532465da4231c7b414158a07d968ed
cradzka <carter.radzka@student.nmt.edu>
2017-09-12 20:34:24 -0600

    Added treeNode

--- frontend/scanType.h
+++ frontend/scanType.h
@@ -45,6 +45,34 @@ typedef struct IDT {
 typedef struct KWT {
     int lineNumber;
     char* tokenType;
-
     char* KWTvalue;
 } KWT;
+
+typedef struct treeNode {
+    //connecivity in the tree
+    struct treeNode *child[MAXCHILDREN]; //children of the node
+    struct treeNode *sibling;            //siblings for the node 
+
+    //what kind of node
+    int lineno;              //linenum 
+    NodeKind nodeKind;       //type of node
+    union {
+        DeclKind decl;       //used when DeclK
+        StmtKind stmt;       //used when StmtK
+        ExpKind exp;         //used when ExpK
+    } kind;
+
+    //extra properties about the node depending on type of the node
+    union {                   //relevent data to type -> attr
+        OpKind op;            //type of token (same as in bison)
+        int value;            //used when an integer constant or boolean
+        unsigned char cvalue; //used when a character
+        char *string;         //used when a sring constant
+        char *name;           //used when Idk
+    } attr;
+
+    ExpType expType;      //used when ExpK for type checking
+    bool isArray;         //is this an array
+    bool isRecord;        //is staticly allocated
+    bool isStatic;        //is staticly allocated 
+} TreeNode;
