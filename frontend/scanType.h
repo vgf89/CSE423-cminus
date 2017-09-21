a2f6d2074bef4705a7035ae62346c16bd220e59f
Angelo Luna <angeloluna@Angelos-MacBook-Pro.local>
2017-09-21 16:48:51 -0600

    separated tree to its own .h file

--- frontend/scanType.h
+++ frontend/scanType.h
@@ -1,5 +1,3 @@
-#define MAXCHILDREN 2048
-
 //NUMCONST Type
 typedef struct NCT {
     int lineNumber;
@@ -49,29 +47,3 @@ typedef struct KWT {
     char* tokenType;
     char* KWTvalue;
 } KWT;
-
-typedef struct treeNode {
-    //connecivity in the tree
-
-    struct treeNode *child[MAXCHILDREN]; //children of the node
-
-    struct treeNode *sibling;            //siblings for the node 
-
-    //what kind of node
-    //NodeKind nodeKind;       //type of node
-
-    //extra properties about the node depending on type of the node
-
-    enum {id, intconst, operators} kind;
-
-    union {                   //relevent data to type -> attr
-        char* id;
-        int intconst;
-        struct {treeNode *left, *right} addE;
-    } val;
-    ExpType expType;      //used when ExpK for type checking
-
-    int isArray;         //is this an array
-    int isRecord;        //is staticly allocated
-    int isStatic;        //is staticly allocated 
-} TreeNode;
---
+++ frontend/tree.h
@@ -0,0 +1,27 @@
+#define MAXCHILDREN 2048
+
+typedef struct treeNode {
+    //connecivity in the tree
+
+    struct treeNode *child[MAXCHILDREN]; //children of the node
+
+    struct treeNode *sibling;            //siblings for the node 
+
+    //what kind of node
+    //NodeKind nodeKind;       //type of node
+
+    //extra properties about the node depending on type of the node
+
+    enum {id, intconst, operators} kind;
+
+    union {                   //relevent data to type -> attr
+        char* id;
+        int intconst;
+        struct {treeNode *left, *right} addE;
+    } val;
+    ExpType expType;      //used when ExpK for type checking
+
+    int isArray;         //is this an array
+    int isRecord;        //is staticly allocated
+    int isStatic;        //is staticly allocated 
+} TreeNode;
