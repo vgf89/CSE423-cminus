a919bffbb212c544d633e1d36eba1d054b0a8f3d
Kevin Hardy <hardy.kevin.c@gmail.com>
2017-09-28 16:59:17 -0600

    Working tests

--- frontend/tests.sh
+++ frontend/tests.sh
@@ -1,11 +1,67 @@
-input='testDataA2/everything06.c-'
-compare='testDataA2/everything06.out'
-DIFF="$(diff <(cat $compare) <(./c- "$input"))"
+#!/bin/bash
+echo "Running Tests..."
+fail=0
 
-if [ "$DIFF" != "" ]
+# Test -p functionality (Assignment 2 output with -p option)
+FILES=testDataA2/*.c-
+for f in $FILES
+do
+    compare=${f/.c-/.out}
+    echo $f
+    echo $compare
+    DIFF="$(diff <(cat $compare) <(./c- -p "$f"))"
+
+    if [ "$DIFF" != "" ]
+    then
+        echo "Output Mismatch ($compare):\n"
+        echo "$DIFF"
+        printf "\n"
+        fail=1
+        exit 1 # Remove once we have small diffs
+    fi
+done
+
+
+# Test default and -P functionality
+FILES=testDataA3/*.c-
+for f in $FILES
+do
+    # default
+    compare=${f/.c-/.out}
+    echo $f
+    echo $compare
+    DIFF="$(diff <(cat $compare) <(./c- "$f"))"
+
+    if [ "$DIFF" != "" ]
+    then
+        echo "Output Mismatch ($compare):\n"
+        echo "$DIFF"
+        printf "\n"
+        fail=1
+        exit 1 # Remove once we have small diffs
+    fi
+
+    # -P
+    compare=${f/.c-/.Pout}
+    echo $f
+    echo $compare
+    DIFF="$(diff <(cat $compare) <(./c- -P "$f"))"
+
+    if [ "$DIFF" != "" ]
+    then
+        echo "Output Mismatch ($compare):\n"
+        echo "$DIFF"
+        printf "\n"
+        fail=1
+        exit 1 # Remove once we have small diffs
+    fi
+done
+
+
+if [ "$fail" == "1" ]
 then
-    echo "Output Mismatch:\n"
-    echo "$DIFF"
     exit 1
 fi
+
+echo "Tests Passed"
 exit 0
