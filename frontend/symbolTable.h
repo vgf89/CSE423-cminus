d2654cf7f4b4cfede9acf794291f9521c8120aef
Kevin Hardy <hardy.kevin.c@gmail.com>
2017-10-06 16:19:20 -0600

    symbol table interface defined

--- frontend/symbolTable.h
+++ frontend/symbolTable.h
@@ -1,93 +1,54 @@
 #ifndef _SYMBOLTABLE_H_
 #define _SYMBOLTABLE_H_
+
 #include <map>
 #include <vector>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
-
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Some sample void * printing routines.   User shoud supply their own.
-//
-void pointerPrintAddr(void *data);
-void pointerPrintLongInteger(void *data);
-void pointerPrintStr(void *data);
 
 
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Introduction
-//
-// This symbol table library supplies basic insert and lookup for
-// symbols linked to void * pointers of data.  Plenty of room for
-// improvement inlcuding: better debugging setup, passing of refs
-// rather than values and purpose built char * routines, and C support.
-// Warning: lookup will return NULL pointer if key is not in table.
-//    This means the void * cannot have zero as a legal value.
-//
-// A main() is commented out and has testing code in it.
-//
-// Robert Heckendorn   Sep 30, 2015
-//
+enum class kind_enum {Var, Func};
 
-
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Class: Scope
-//
-
-// feel free to replace std::string  with std::string
-class Scope {
-private:
-    static bool debugFlg;                      // turn on tedious debugging
-    std::string name;                          // name of scope
-    std::map<std::string , void *> symbols;    // use an ordered map (not as fast as unordered)
-
-public:
-    Scope(std::string newname);
-    ~Scope();
-    void debug(bool state);                    // sets the debug flag to new state
-    void print(void (*printData)(void *));     // prints the table using the supplied function to print the void *
-    void applyToAll(void (*action)(std::string , void *));  // applies func to all symbol/data pairs
-    bool insert(std::string sym, void *ptr);   // inserts a new ptr associated with symbol sym 
-                                               // returns false if already defined
-    void *lookup(std::string sym);             // returns the ptr associated with sym
-                                               // returns NULL if symbol not found
-    std::string scopeName() {return name; };   // accessor for name
+class Entry {
+    /* name */
+    //string name (already key of map)
+    /* type */
+    std::string type; //In case of Record, name of the record type (i.e. Point)
+    /* attribute */
+    kind_enum kind ;
+    bool isStatic;
+    bool isArray;
+    bool isRecord;
 };
 
+class Scope {
+public:
+    int insertSymbol(std::string name, std::string type, kind_enum kind, bool isStatic, bool isArray, bool isRecord); /*
+        Add symbol to scope. Return 0 on succcess, 1 on failure (symbol already exists).
+        If debugging print new entry
+    */
+    bool search(std::string name); //Tries to get string name from symbols map. If found, return true, otherwise false
 
-
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Class: SymbolTable
-//
-// Is a stack of scopes.   The global scope is created when the table is
-// is constructed and remains for the lifetime of the object instance.
-//
+    std::map<std::string, Entry> symbols; //string is name of the symtable entry
+private:
+};
 
 class SymbolTable {
-private:
-    std::vector<Scope *> stack;
-    bool debugFlg;
-
 public:
-    SymbolTable();
-    void debug(bool state);                          // sets the debug flags
-    int depth();                                     // what is the depth of the scope stack?
-    void print(void (*printData)(void *));           // print all scopes using data printing function
-    void applyToAllGlobal(void (*action)(std::string , void *));  // applies func to all symbol/data pairs
-    void enter(std::string name);                    // enter a scope with given name
-    void leave();                                    // leave a scope (not allowed to leave global)
-    void *lookup(std::string sym);                   // returns ptr associated with sym anywhere in symbol table
-                                                     // returns NULL if symbol not found
-    void *lookupGlobal(std::string sym);             // returns ptr associated with sym in globals
-                                                     // returns NULL if symbol not found
-    bool insert(std::string sym, void *ptr);         // inserts new ptr associated with symbol sym in current scope
-                                                     // returns false if already defined
-    bool insertGlobal(std::string sym, void *ptr);   // inserts a new ptr associated with symbol sym 
-                                                     // returns false if already defined
+    SymbolTable(bool debug); // Make new symboltable, initializes the global scope
+    void newScope(); // Next (child) scope. new Scope is made inside this function and pushed to stack, if debug print new scope depth
+    int insertSymbol(std::string name, std::string type, kind_enum kind, bool isStatic, bool isArray, bool isRecord); // Add symbol to current scope (top of stack)
+    int pop();  // Removes and frees(?) current scope, return int for errors (0 = success, 1 = already popped global)
+
+    /* Assuming children can redeclare variable... */
+    bool searchCurrent(std::string name); // Used for checking during declaration (true if found, false otherwise)
+    bool searchAll(std::string name); // Used for checking during type checks (true if found, false otherwise)
+
+private:
+    bool debug;
+    int depth(); // Just returns stack.length();
+    std::vector<Scope *> stack; // stack.at(0) is the global scope
+
 };
+
 
 #endif /* _SYMBOLTABLE_H_ */
