fc9b486e4add16cffd33371b2dbc9f0aaa210c7b
cradzka <carter.radzka@student.nmt.edu>
2017-10-08 16:38:18 -0600

    Finished error prints

--- frontend/annotated.h
+++ frontend/annotated.h
@@ -18,7 +18,20 @@ void treeTraverse(treeNode *curTree);
 
 
 // Error printing functions
-void printSymbolAlreadyDefinedError(int linenum, char* s, int ln);
-void printSymbolNotDefinedError(int linenum, char* s);
+void printSymbolAlreadyDefinedError(int linenum1, char* symbol, int linenum2);
+void printSymbolNotDefinedError(int linenum, char* symbol);
+void simpleVarCalledError(int linenum, char* var); 
+void requiredOpLhsError(int linenum, char* reqType, char* givenType);
+void requiredOpRhsError(int linenum, char* reqType, char* givenType);
+void operandTypeMistmatchError(int linenum, char* givenType, char *lhType, char *rhType);
+void arrayIndexTypeError(int linenum, char* reqType, char* givenType);
+void unindexedArrayError(int linenum, char* array);
+void indexingNamedNonArrayError(int linenum, char* array);
+void indexingUnamedNonArrayError(int linenum);
+void returnArrayError(int linenum);
+void functionAsVariableError(int linenum, char* func);
+void invalidArrayOperationError(int linenum, char* op);
+void opOnlyForArraysError(int linenum, char* op);
+void invalidUnaryOpError(int linenum, char* reqOp, char* givenOp);
 
 #endif /* ANNOTATED_H */
